d3fd6c3e8ecd16964f3db8235f550eefd72e185c
Jakob Murko <sraka1@users.noreply.github.com>
2022-01-22 23:34:02 +0100

    Remove update and logging processes

--- selfdrive/controls/lib/drive_helpers.py
+++ selfdrive/controls/lib/drive_helpers.py
@@ -7,9 +7,9 @@ from selfdrive.modeld.constants import T_IDXS
 
 # WARNING: this value was determined based on the model's training distribution,
 #          model predictions above this speed can be unpredictable
-V_CRUISE_MAX = 145  # kph
+V_CRUISE_MAX = 155  # kph
 V_CRUISE_MIN = 8  # kph
-V_CRUISE_ENABLE_MIN = 40  # kph
+V_CRUISE_ENABLE_MIN = 30  # kph
 
 LAT_MPC_N = 16
 LON_MPC_N = 32
